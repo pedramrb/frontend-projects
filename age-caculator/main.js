636b72d3baa76eccd537290c624eef1315cad0fd
Pedram <eng.pedram.r@gmail.com>
2023-07-14 16:24:59 +0330

    complete js file

--- age-caculator/main.js
+++ age-caculator/main.js
@@ -1,1 +1,27 @@
+const btn = document.getElementById("btn")
+const birthele = document.getElementById("birthday")
+const resultele = document.getElementById("result")
 
+function calculateAge(){
+    const birthdayValue = birthele.value;
+    if(birthdayValue === ""){
+        alert("Please enter your birthday!")
+    }else{
+        const age = getAge(birthdayValue);
+        resultele.innerText = `Your age is : ${age} ${age>1 ? "years" : "year"} old`;
+    }
+}
+
+
+function getAge(birthday){
+    const currentDate = new Date();
+    const birthdayDate = new Date(birthday)
+    let age = currentDate.getFullYear() - birthdayDate.getFullYear()
+    const month = currentDate.getMonth() - birthdayDate.getMonth()
+    if(month<0 || (month === 0 && currentDate.getDate()<birthdayDate.getDate())){
+        age--;
+    }
+    return age;
+}
+
+btn.addEventListener("click", calculateAge);

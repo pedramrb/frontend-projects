a1a8bf2a6119c57cf69185f17fb599757743bac7
Pedram <eng.pedram.r@gmail.com>
2023-07-17 14:31:28 +0330

    search bar project completed

--- animated-search-bar/main.js
+++ animated-search-bar/main.js
@@ -1,1 +1,7 @@
 
+const searchBarContainerele = document.querySelector(".search-bar-container");
+const magnifierele = document.querySelector(".magnifier");
+
+magnifierele.addEventListener("click", () =>{
+    searchBarContainerele.classList.toggle("active")
+})
